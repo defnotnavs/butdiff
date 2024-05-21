6710a3a74e796eb42be730359fd652cd932354c1
defnotnavs <navijonas28@gmail.com>
2024-05-21 15:27:13 +0800

    Add files via upload

---
+++ CANTOS,IVAN_JONAS_S/MasMalinis/functioncalt.js
@@ -0,0 +1,28 @@
+function add1(p1, p2) {
+    return Number(p1) + Number(p2);
+  }
+  
+  
+
+  function sub1(p1, p2) {
+    return Number(p1) - Number(p2);
+  }
+  
+ 
+  function mult1(p1, p2) {
+    return Number(p1) * Number(p2);
+  }
+  
+
+
+  function div1(p1, p2) {
+    if (Number(p2) === 0 ) {
+        return 'baliw ka ba divide by 0 ampt'
+    } else {
+        return Number(p1) / Number(p2);
+    }
+  }
+  
+ 
+   
+   
